2f67b8ae95332a6eacb1af6e2eae60dbcfad995c
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2024-07-30 22:16:31 -0300

    utilizando mais do polimorfismo
    aplicando mais ainda do polimorfismo para entender

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -17,9 +19,10 @@ public class Principal {
 //        System.out.println(meuFilme.anoDeLancamento);
 
         meuFilme.exibeFichaTecnica();
-        meuFilme.avalia(9);
-        meuFilme.avalia(9);
-        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(10);
+        meuFilme.avalia(10);
+        meuFilme.avalia(10);
 
         System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
 
@@ -45,5 +48,15 @@ public class Principal {
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
 
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+
+        filtro.filtra(meuFilme);
+
+        Episodio episodio = new Episodio();
+
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 }
