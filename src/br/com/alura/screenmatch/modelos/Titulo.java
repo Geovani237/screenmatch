b25afa3cd45f7036867b3ec91d3dbff1484f5e06
Geovani carlos de souza <94084031+Geovani237@users.noreply.github.com>
2025-05-06 11:31:31 -0300

    adicionando exception

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,57 +1,70 @@
 package br.com.alura.screenmatch.modelos;
 
-import com.google.gson.annotations.SerializedName;
+import br.com.alura.screenmatch.exception.ErroDeConversaoDeAnoException;
 
-public class Titulo {
-    @SerializedName("Title")
-    private String nome;// atributos de publicos não vai existir.
-    @SerializedName("Year")
+public class Titulo implements Comparable<Titulo> {
+    private String nome;
     private int anoDeLancamento;
-    private int duracaoEmMinutos;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
 
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
     }
 
-    //Metodo acessor
-    public int getTotalDeAvaliacoes() {
+    public Titulo(TituloOmdb meuTituloOmdb) {
+        this.nome = meuTituloOmdb.title();
+        if (meuTituloOmdb.year().length() > 4) {
+            throw new ErroDeConversaoDeAnoException("Não consegui converter o ano");
+        }
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmdb.year());
+        this.duracaoEmMinutos = Integer.valueOf(meuTituloOmdb.runtime().substring(0,2));
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public int getTotalDeAvaliacoes() {
         return totalDeAvaliacoes;
     }
-    public int getAnoDeLancamento() {
-        return this.anoDeLancamento;
-    }
-    public boolean getIncluidoNoPlano() {
-        return this.incluidoNoPlano;
-    }
-    public int getDuracaoEmMinutos(){
-        return this.duracaoEmMinutos;
-    }
-    public String getNome() {
-        return this.nome;
-    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
-    public void setAnoDeLancamento(int anoDeLancamento){
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
         this.anoDeLancamento = anoDeLancamento;
     }
-    public void setDuracaoEmMinutos(int duracaoEmMinutos){
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
+
     public void setIncluidoNoPlano(boolean incluidoNoPlano) {
         this.incluidoNoPlano = incluidoNoPlano;
     }
 
-    //Método só vai fazer alguma coisa e não tem algum retorno vamos decladar da seguinte forma:
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
     public void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome); // forma de instanciar Objeto
+        System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
+
     public void avalia(double nota){
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
@@ -62,9 +75,15 @@ public class Titulo {
     }
 
     @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
     public String toString() {
-        return  "nome=" + nome +
-                ", anoDeLancamento='" + anoDeLancamento + '\'' +
-                '}';
+        return "Titulo " +
+                "nome = '" + nome + '\'' +
+                ", anoDeLancamento = " + anoDeLancamento +
+                ", duracaoEmMinutos = " + duracaoEmMinutos;
     }
 }
--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -1,37 +1,74 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.exception.ErroDeConversaoDeAnoException;
 import br.com.alura.screenmatch.modelos.Titulo;
+import br.com.alura.screenmatch.modelos.TituloOmdb;
+import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Digite o nome do filme: ");
-        var busca = scanner.nextLine();
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=a4e48616";
+    public static void main (String[] args) throws IOException, InterruptedException {
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
+        Scanner leitura = new Scanner(System.in);
 
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Digite um filmes para busca: ");
 
-        String json = response.body();
-        System.out.println(json);
+        var busca = leitura.nextLine();
+
+        String modificado = URLEncoder.encode(busca, StandardCharsets.UTF_8);
+
+        String endereco = "https://www.omdbapi.com/?t=" + modificado + "&apikey=6585022c";
+
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(endereco))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+
+            String json = response.body();
+
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+            //Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+            TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+            System.out.println(meuTituloOmdb);
+
+            //try {
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Titulo já convertido: ");
+            System.out.println(meuTitulo);
+
+            FileWriter escrita = new FileWriter("filmes.txt");
+            escrita.write(meuTitulo.toString());
+            escrita.close();
+        } catch (NumberFormatException e) {
+            System.out.println("Aconteceu um erro!!");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Algum erro de argumento na busca");
+            System.out.println(e.getMessage());
+        } catch (ErroDeConversaoDeAnoException e) {
+            System.out.println(e.getMessage());
+        }
 
 
-        Gson gson = new Gson();
-        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
 
-        System.out.println("Titulo: " + meuTitulo);
+
+
     }
 }
